4f21400c8099e2e9afccc4f0cfdf9d3acfb060ac
formicola-davide <formicola.davide@iisgalvanimi.edu.it>
2022-11-10 11:23:00 +0000

    versione 1

---
+++ main_1.py
@@ -0,0 +1,3 @@
+username = input("Enter username:")   #dato che la versione di python è la 3.6, ti permette di creare una variabile che scrivi nella console comandi.
+print("Username is: " + username)
+
